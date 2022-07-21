401f76ce2577e4cb36a2eca355455c9f9939f586
jeroalvarez1 <jeroalvarez1@gmail.com>
2022-07-21 12:40:58 -0300

    Tercer commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -22,21 +22,30 @@ export class LoginPage implements OnInit {
   }
 
   async ingresar() {
-
-    var formulario = this.formularioLogin.value;
-    var usuario = JSON.parse(localStorage.getItem('usuario'));
-    if (usuario.nombre == formulario.nombre && usuario.password == formulario.password) {
-      localStorage.setItem('ingresado', 'true');
-      this.navController.navigateRoot('home');
-    } else {
+    if (this.formularioLogin.invalid) {
       const alert = await this.alertController.create({
-        header: 'Datos incorrectos',
-        message: 'Los datos que ingresaste no son correctos',
+        header: 'Datos incompletos',
+        message: 'Tienes que llenar todos los campos',
         buttons: ['Aceptar']
       });
-
       await alert.present();
       return;
-    }
+    } else {
+      var formulario = this.formularioLogin.value;
+      var usuario = JSON.parse(localStorage.getItem('usuario'));
+      if (usuario.nombre == formulario.nombre && usuario.password == formulario.password) {
+        localStorage.setItem('ingresado', 'true');
+        this.navController.navigateRoot('home');
+      } else {
+        const alert = await this.alertController.create({
+          header: 'Datos incorrectos',
+          message: 'Los datos que ingresaste no son correctos',
+          buttons: ['Aceptar']
+        });
+
+        await alert.present();
+        return;
+      }
   }
+}
 }
